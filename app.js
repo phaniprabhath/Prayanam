fd4c9dff50cdea276fe26924c1605dc6e8e9c944
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-09-25 20:05:12 +0530

    use of ejs-mate

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path = require("path");  // its necessary for ejs
 const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+// ejs-mate helps in creating templates or layouts. It's a 'npm' package.
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -23,6 +25,7 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate); // for ejs-mate
 
 app.get("/",(req,res)=>{                   
     res.send("Hi, I am root");
