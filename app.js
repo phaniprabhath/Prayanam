8c9b0e98c2fa2ea5078584fa9408fc9e61e00d85
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-09-27 12:10:01 +0530

    started using bootstrap

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate); // for ejs-mate
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{                   
     res.send("Hi, I am root");
