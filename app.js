534c322578bc964437e1aaa79aef8c782c0d83d5
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-10-11 12:32:13 +0530

    added error handling middleware

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");  // its necessary for ejs
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 // ejs-mate helps in creating templates or layouts. It's a 'npm' package.
-
+const wrapAsync=require("./utils/wrapAsync.js");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -50,11 +50,14 @@ app.get("/listings/:id",async (req,res)=>{
     res.render("listings/show.ejs",{listing});
 });
 // Create Route
-app.post("/listings",async (req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post(
+    "/listings",
+    wrapAsync(async (req,res,next)=>{
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}));
+
 // Edit Route
 app.get("/listings/:id/edit", async (req,res)=>{
     let {id}=req.params;
@@ -76,6 +79,11 @@ app.delete("/listings/:id",async(req,res)=>{
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 });
+
+// Middleware
+app.use((err,req,res,next)=>{
+    res.send("something went wrong!");
+})
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
