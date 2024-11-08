0d308a6f4fcf47064ee289dbd2a2b20db34e0c9e
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-11-08 19:20:33 +0530

    added user SignUp form

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const ejsMate=require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
-const listings= require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+const listingRouter= require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -51,6 +55,13 @@ app.get("/",(req,res)=>{
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success"); //res.locals() is to store variables
     res.locals.error=req.flash("error");
@@ -58,8 +69,9 @@ app.use((req,res,next)=>{
 });
 
 // These two lines are for using Router object
-app.use("/listings",listings); 
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter); 
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 // The below code is for throwing the error if the user searches none of the above routes.
 app.all("*",(req,res,next)=>{
