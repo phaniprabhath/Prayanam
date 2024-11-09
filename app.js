2774a1e39a254d4d5ffafa066c0b93291b832449
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-11-09 10:26:14 +0530

    made sure that login & logout doesn't appear at a time

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success"); //res.locals() is to store variables
     res.locals.error=req.flash("error");
+    res.locals.currUser=req.user;
     next();
 });
 
