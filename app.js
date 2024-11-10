c2288bb19932a42ea999d35b1521337cb9372f12
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-11-10 22:09:18 +0530

    modified app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store=MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secret:"mysupersecretcode"
+        secret:process.env.SECRET
     },
     touchAfter:24*3600
 });
@@ -55,7 +55,7 @@ store.on("error",()=>{
 });
 const sessionOptions={
     store:store,
-    secret:"mysupersecretcode",
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized: true,
     cookie:{
