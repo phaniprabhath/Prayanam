5808b7f04753d5e6b285ed486808ec6b86828022
Phani Prabhath <phaniprabhath.dheekonda@gmail.com>
2024-11-10 10:11:29 +0530

    added owner to the present listings

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main(){
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({
+        ...obj,
+        owner:"673031eff6ec44c41c0a0a89"
+    }));
     await Listing.insertMany(initData.data);
     console.log("data was initialised");
 };
